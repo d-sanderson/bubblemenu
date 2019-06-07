318252352a40e7194c708b11d9929cf2a146d318
dsanderson90 <dsanderson3@cnm.edu>
2019-06-07 00:28:10 -0600

    added desserts

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ let apps = document.querySelector('.apps');
 let fbs = document.querySelector('.fbs');
 let sandwiches = document.querySelector('.sandwiches');
 let bowls = document.querySelector('.bowls');
+let desserts = document.querySelector('.desserts');
 
 let appsArr = ['Brussells', 'Harissa Dip', 'Hummus Trio', 'Zuchini Ribbons', 'Calamari', 'Baked Brie', 'Really Good Fries', 'Blistered Shishitos']
 let fbsArr = ['Margharita', 'Curried Cauliflower', 'Pork Belly and Blue Cheese', 'Steaks and Ports']
 let bowlsArr = ['Korean Fried Chicken', 'Salmon and Himalayan Rice', 'Harissa Portobella', 'New Mexico Shrimp & Grits']
 let wichesArr = ['Chimichurri Chicken', 'Patty Melt', 'Grilled Mozzarella & Portabella', 'Sixty-Six Green Chile Cheeseburger', 'Crispy Pork Belly Bahn Mi', 'Pastrami Rueben']
+let dessertsArr = ['Chocolate and Whipped Cream Pudding', 'Bananna ShortBread', 'Green Chile Apple Empanada',  ]
 
 for (var i = 0; i < appsArr.length; i++) {
-let div = document.createElement('div');
-div.className = 'menu-item'
-div.setAttribute('data-clicks', 0)
-div.value = appsArr[i]
-div.innerText = appsArr[i]
-apps.appendChild(div);
+  let div = document.createElement('div');
+  div.className = 'menu-item'
+  div.setAttribute('data-clicks', 0)
+  div.value = appsArr[i]
+  div.innerText = appsArr[i]
+  apps.appendChild(div);
 }
 
 for (var i = 0; i < fbsArr.length; i++) {
@@ -34,6 +36,7 @@ for (var i = 0; i < wichesArr.length; i++) {
   div.innerText = wichesArr[i]
   sandwiches.appendChild(div);
 }
+
 for (var i = 0; i < bowlsArr.length; i++) {
   let div = document.createElement('div');
   div.className = 'menu-item'
@@ -43,23 +46,30 @@ for (var i = 0; i < bowlsArr.length; i++) {
   bowls.appendChild(div);
 }
 
+for (var i = 0; i < dessertsArr.length; i++) {
+  let div = document.createElement('div');
+  div.className = 'menu-item'
+  div.setAttribute('data-clicks', 0)
+  div.value = dessertsArr[i]
+  div.innerText = dessertsArr[i]
+  desserts.appendChild(div);
+}
+
 function click(e) {
   if (!e.target.getAttribute('data-clicks'))
     e.target.setAttribute('data-clicks', 0);
   var clicks = parseInt(e.target.getAttribute('data-clicks'));
   e.target.setAttribute('data-clicks', clicks + 1);
-  e.target.textContent = `${this.value} ${clicks + 1}`;
 }
 
 function sortItems(e) {
   let menuItems = jQuery(e.target.parentNode.children)
   let classname = e.target.parentNode.className
   menuItems.sort(function(a, b){ return $(b).data("clicks")-$(a).data("clicks")});
-  $('.'+classname).html(menuItems)
+  $('.' + classname).html(menuItems)
   }
 
 let items = document.querySelectorAll('.menu-item')
-
 items.forEach(item => item.addEventListener('click', click))
 items.forEach(item => item.addEventListener('click', sortItems))
 
